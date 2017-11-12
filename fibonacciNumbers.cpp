7e9b70ed56b56cbc812503f80c7e186a87768027
arimmy <33543566+arimmy@users.noreply.github.com>
2017-11-12 12:05:03 +0200

    Update fibonacciNumbers.cpp

--- fibonacciNumbers.cpp
+++ fibonacciNumbers.cpp
@@ -10,6 +10,8 @@ int fibonacci_recursion(int n)
 	return fibonacci_recursion(n - 1) + fibonacci_recursion(n - 2);
 }
 //complexity -> O(2^n)
+//the sequence of numbers starts from 0
+
 
 int fibonacci_linear(int n)
 {
@@ -25,18 +27,57 @@ int fibonacci_linear(int n)
 	return number;
 }
 //complexity -> O(n)
+//the sequence of numbers starts from 0
 
-//complexity -> O(log n):
-//If N is even => k=n/2 :    F(n) = [2*F(k-1) + F(k)] * F(k);
-//If N is odd => k=(n+1)/2 :    F(n) = F(k)*F(k) + F(k-1)*F(k-1);
+
+void multiply(int F[2][2], int M[2][2])
+{
+	int x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
+	int y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
+	int z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
+	int w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
+
+	F[0][0] = x;
+	F[0][1] = y;
+	F[1][0] = z;
+	F[1][1] = w;
+}
+
+void power(int F[2][2], int n)
+{
+	if (n == 0 || n == 1)
+		return;
+	int M[2][2] = { { 1,1 },{ 1,0 } };
+
+	power(F, n / 2);
+	multiply(F, F);
+
+	if (n % 2 != 0)
+		multiply(F, M);
+}
+
+
+int fibonacci_matrix(int n)
+{
+	int F[2][2] = { { 1,1 },{ 1,0 } };
+	if (n == 0)
+		return 0;
+	power(F, n - 1);
+	return F[0][0];
+}
+//complexity -> O(log n)
+//the sequence of numbers starts from 1
+
 
 int main()
 {
-	int a, b;
+	int a, b, c;
 	a = fibonacci_recursion(3);
 	b = fibonacci_linear(3);
+	c = fibonacci_matrix(3);
 	cout << a << endl;
 	cout << b << endl;
+	cout << c << endl;
 
 	system("pause");
 	return 0;

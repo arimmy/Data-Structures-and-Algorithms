72077e538d495510be3838217f9374157872d211
arimmy <33543566+arimmy@users.noreply.github.com>
2017-11-12 16:47:59 +0200

    Update GameOfTwoStacks.cpp

--- GameOfTwoStacks.cpp
+++ GameOfTwoStacks.cpp
@@ -1,128 +1,7 @@
 #include<iostream>
+#include<stack>
 
 using namespace std;
-
-template <typename T>
-struct node {
-	T data;
-	node* next;
-};
-
-template <typename T>
-class Lstack {
-private:
-	node<T>*  topNode;
-
-	void copy(node<T>*);
-	void copyLstack(const Lstack&);
-	void deleteLstack();
-public:
-	Lstack();
-	Lstack(const Lstack&);
-	Lstack& operator=(const Lstack&);
-	~Lstack();
-
-	bool empty() const;
-	void push(const T&);
-	T pop();
-	T top() const;
-};
-
-
-template <typename T>
-bool Lstack<T>::empty() const
-{
-	return topNode == nullptr;
-}
-
-template <typename T>
-void Lstack<T>::copy(node<T>* toCopy)
-{
-	if (toCopy == nullptr)
-		return;
-	copy(toCopy->next);
-	push(toCopy->data);
-}
-
-template <typename T>
-void Lstack<T>::copyLstack(Lstack<T> const & other)
-{
-	topNode = nullptr;
-	copy(other.topNode);
-}
-
-template <typename T>
-void Lstack<T>::deleteLstack()
-{
-	while (!empty())
-		pop();
-}
-
-template <typename T>
-Lstack<T>::Lstack() :topNode(nullptr)
-{}
-
-template <typename T>
-Lstack<T>::Lstack(const Lstack<T>& other)
-{
-	copyLstack(other);
-}
-
-template <typename T>
-Lstack<T>& Lstack<T>::operator=(const Lstack<T>& other)
-{
-	if (this != &other)
-	{
-		deleteLstack();
-		copyLstack(other);
-	}
-
-	return *this;
-}
-
-template <typename T>
-Lstack<T>::~Lstack()
-{
-	deleteLstack();
-}
-
-template <typename T>
-void Lstack<T>::push(const T& x)
-{
-	node<T>* p = new node<T>;
-	p->data = x;
-	p->next = topNode;
-	topNode = p;
-}
-
-template <typename T>
-T Lstack<T>::pop()
-{
-	if (empty())
-	{
-		cerr << "Stack is empty!\n";
-		return 0;
-	}
-
-	node<T>* p = topNode;
-	topNode = topNode->next;
-	T x = p->data;
-	delete p;
-	return x;
-
-}
-
-template <typename T>
-T Lstack<T>::top() const
-{
-	if (empty())
-	{
-		cerr << "Stack is empty!\n";
-		return 0;
-	}
-
-	return topNode->data;
-}
 
 int main()
 {
@@ -131,7 +10,7 @@ int main()
 
 	cin >> games;
 	
-	Lstack<int> stack1, stack2;
+	stack<int> stack1, stack2;
 
 	for (int i = 0; i < games; ++i)
 	
